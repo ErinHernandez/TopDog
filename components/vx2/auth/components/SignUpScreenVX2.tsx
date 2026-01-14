674cf17bda2226159aa24a5c39c6580a332451a3
ErinHernandez <teddy.gurskis@gmail.com>
2026-01-14 15:23:25 -0500

    fix: Update inputRef prop type to accept nullable HTMLInputElement in SignUpScreenVX2

--- components/vx2/auth/components/SignUpScreenVX2.tsx
+++ components/vx2/auth/components/SignUpScreenVX2.tsx
@@ -114,7 +114,7 @@ interface InputProps {
   disabled?: boolean;
   error?: string | null;
   touched?: boolean;
-  inputRef?: React.RefObject<HTMLInputElement>;
+  inputRef?: React.RefObject<HTMLInputElement | null>;
   onBlur?: () => void;
   rightElement?: React.ReactNode;
   autoFocus?: boolean;
