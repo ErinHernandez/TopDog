be404f61f51f0167542f2ab8f18853868cea5341
ErinHernandez <teddy.gurskis@gmail.com>
2026-01-14 14:53:00 -0500

    Fix TypeScript error: Update inputRef prop type to accept nullable HTMLInputElement

--- components/vx2/auth/components/LoginScreenVX2.tsx
+++ components/vx2/auth/components/LoginScreenVX2.tsx
@@ -131,7 +131,7 @@ interface InputProps {
   disabled?: boolean;
   error?: string | null;
   touched?: boolean;
-  inputRef?: React.RefObject<HTMLInputElement>;
+  inputRef?: React.RefObject<HTMLInputElement | null>;
   onBlur?: () => void;
   onFocus?: () => void;
   rightElement?: React.ReactNode;
